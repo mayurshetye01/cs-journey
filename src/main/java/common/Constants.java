c343f6e7cde6e1d45052d99309d4b44df93844b6
Mayur <mayur.shetye@netcracker.com>
2020-04-22 23:22:33 +0530

    Implemented max subarray finder

---
+++ src/main/java/common/Constants.java
@@ -0,0 +1,7 @@
+package common;
+
+public class Constants {
+    public static final String MAX_SUM_KEY = "max_sum";
+    public static final String LOWEST_INDEX_KEY = "lowest_index";
+    public static final String HIGHEST_INDEX_KEY = "highest_index";
+}
